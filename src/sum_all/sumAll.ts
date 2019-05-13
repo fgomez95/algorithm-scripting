688a516ab6d2d7c579c5ec083e47f18b5d4f26eb
Fernando G <fgomez@masterdevel.com>
2019-05-12 21:59:31 -0600

    added array symetric difference and refactored code

---
+++ src/sum_all/sumAll.ts
@@ -0,0 +1,11 @@
+export default function sumAll(arr: Array<number>): number {
+  let arrCopy: Array<number> = [];
+  const x:number = arr[0];
+  const y:number = arr[1];
+  x > y
+    ? arrCopy.push(y, x)
+    : arrCopy = [...arr];
+  let result:number = arrCopy[0];
+  for (let i = arrCopy[0]; i < arrCopy[1]; i += 1)result += i + 1;
+  return result;
+}
