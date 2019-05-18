7b178e658c52c5315621a98335faedc5516f3bcb
Fernando Gomez <fgomez.sierra95@gmail.com>
2019-05-18 11:00:26 -0600

    completed where art thou

---
+++ src/where_are_thou/whereArtThou.ts
@@ -0,0 +1,15 @@
+interface normalizeInterface {
+    [key:string]: any;
+}
+export default function whereArtThou(x: Array<normalizeInterface>, y: normalizeInterface): Array<any>{
+    let res:Array<normalizeInterface>;
+    let keys:Array<string> = Object.keys(y);
+    res = x.filter((el:normalizeInterface)=>{
+        let flag:boolean = true;
+        keys.forEach((key:string) => {
+            if(el[key] !== y[key]) flag = false;
+        });
+        return flag;
+    });
+    return res;
+}
